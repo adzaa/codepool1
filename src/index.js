07ae8269e759f5da94b2a43ce061e01b8f0b7eb2
adzaa <adidzankic@gmail.com>
2021-09-08 15:46:03 +0200

    f commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import MovieAppProvider from './context/MovieContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MovieAppProvider>
+      <App />
+    </MovieAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
